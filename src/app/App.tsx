2c50be8aca9c1305be1d987fdb4a94ff90fdbfd1
Adam Norris <anorris@rivasolutionsinc.com>
2022-02-13 18:32:48 +0000

    Reverted message

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ export const App: FunctionComponent = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload -- eWAPS-MOD.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
